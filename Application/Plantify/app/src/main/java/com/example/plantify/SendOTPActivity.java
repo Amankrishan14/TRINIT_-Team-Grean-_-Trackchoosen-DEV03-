60683a0d1cecd3d1fac083a12b69ace7545b8f6c
tarun028 <tarunsrivastavakvs@gmail.com>
2023-02-12 07:23:52 +0530

    final app

--- Application/Plantify/app/src/main/java/com/example/plantify/SendOTPActivity.java
+++ Application/Plantify/app/src/main/java/com/example/plantify/SendOTPActivity.java
@@ -1,8 +1,20 @@
 package com.example.plantify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+
+
+import java.util.Currency;
+import java.util.concurrent.TimeUnit;
 
 public class SendOTPActivity extends AppCompatActivity {
 
@@ -10,5 +22,6 @@ public class SendOTPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_otpactivity);
+
     }
 }
